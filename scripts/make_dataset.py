fc86339f9936ed21723dbe0bf9f9f8054810bcf2
Pablo Rosado <pabloarosado@gmail.com>
2023-12-26 18:14:38 +0100

    Improve main script to create a better codebook, add codebook to excel file, and save to json in shorter time

--- scripts/make_dataset.py
+++ scripts/make_dataset.py
@@ -6,15 +6,16 @@ Running this script will generate the full energy dataset in three different for
 * owid-energy-data.json
 
 """
-
 import argparse
 import json
-from typing import List
+import re
+from pathlib import Path
 
 import pandas as pd
-from owid import catalog
+from owid.catalog import LocalCatalog, Origin, Table, find
 
-from scripts.shared import OUTPUT_DIR
+# Define path to output directory.
+OUTPUT_DIR = Path(__file__).parent.parent
 
 # Define paths to output files.
 OUTPUT_CSV_FILE = OUTPUT_DIR / "owid-energy-data.csv"
@@ -25,100 +26,203 @@ CODEBOOK_FILE = OUTPUT_DIR / "owid-energy-codebook.csv"
 ENERGY_DATASET_TABLE = "owid_energy"
 
 
-def df_to_json(
-    complete_dataset: pd.DataFrame, output_path: str, static_columns: List[str]
-) -> None:
-    megajson = {}
+def save_data_to_json(tb: Table, output_path: str) -> None:
+    tb = tb.copy()
 
-    # Round all values to 3 decimals.
-    rounded_cols = [
-        col
-        for col in list(complete_dataset)
-        if col not in ("country", "year", "iso_code")
-    ]
-    complete_dataset[rounded_cols] = complete_dataset[rounded_cols].round(3)
+    # Initialize output dictionary, that contains one item per country in the data.
+    output_dict = {}
 
-    for _, row in complete_dataset.iterrows():
+    # Each country contains a dictionary, which contains:
+    # * "iso_code", which is the ISO code (as a string), if it exists.
+    # * "data", which is a list of dictionaries, one per year.
+    #   Each dictionary contains "year" as the first item, followed by all other non-nan indicator values for that year.
+    for country in sorted(set(tb["country"])):
+        # Initialize output dictionary for current country.
+        output_dict[country] = {}
 
-        row_country = row["country"]
-        row_dict_static = row.drop("country")[static_columns].dropna().to_dict()
-        row_dict_dynamic = row.drop("country").drop(static_columns).dropna().to_dict()
+        # If there is an ISO code for this country, add it as a new item of the dictionary.
+        iso_code = tb[tb["country"]==country].iloc[0]["iso_code"]
+        if not pd.isna(iso_code):
+            output_dict[country]["iso_code"] = iso_code
 
-        if row_country not in megajson:
-            megajson[row_country] = row_dict_static
-            megajson[row_country]["data"] = [row_dict_dynamic]
-        else:
-            megajson[row_country]["data"].append(row_dict_dynamic)
+        # Create the data dictionary for this country.
+        dict_country = tb[tb["country"] == country].drop(columns=["country", "iso_code"]).to_dict(orient="records")
+        # Remove all nans.
+        data_country = [{indicator:value for indicator, value in d_year.items() if not pd.isna(value)} for d_year in dict_country]
+        output_dict[country]["data"] = data_country
 
+    # Write dictionary to file as a big json object.
     with open(output_path, "w") as file:
-        file.write(json.dumps(megajson, indent=4))
+        file.write(json.dumps(output_dict, indent=4))
 
 
-def prepare_data(table: catalog.Table) -> pd.DataFrame:
-    # Create a dataframe with a dummy index from the table.
-    df = pd.DataFrame(table).reset_index()
-
+def prepare_data(tb: Table) -> Table:
     # Sort rows and columns conveniently.
-    df = df.sort_values(["country", "year"]).reset_index(drop=True)
-    first_columns = ["country", "year", "iso_code"]
-    df = df[
-        first_columns + [column for column in df.columns if column not in first_columns]
+    tb = tb.reset_index().sort_values(["country", "year"]).reset_index(drop=True)
+    first_columns = ["country", "year", "iso_code", "population", "gdp"]
+    tb = tb[
+        first_columns
+        + [column for column in sorted(tb.columns) if column not in first_columns]
     ]
 
-    return df
+    return tb
 
 
-def prepare_codebook(table: catalog.Table) -> pd.DataFrame:
-    table = table.reset_index()
+def remove_details_on_demand(text: str) -> str:
+    # Remove references to details on demand from a text.
+    # Example: "This is a [description](#dod:something)." -> "This is a description."
+    regex = r"\(\#dod\:.*\)"
+    if "(#dod:" in text:
+        text = re.sub(regex, "", text).replace("[", "").replace("]", "")
 
-    # Gather column names, descriptions and sources from the variables' metadata.
-    metadata = {"column": [], "description": [], "source": []}
+    return text
+
+
+def prepare_codebook(tb: Table) -> pd.DataFrame:
+    table = tb.copy()
+
+    # Manually create an origin for the regions dataset.
+    regions_origin = [Origin(producer="Our World in Data", title="Regions", date_published=str(table["year"].max()))]
+
+    # Manually edit some of the metadata fields.
+    table["country"].metadata.title = "Country"
+    table["country"].metadata.description_short = "Geographic location."
+    table["country"].metadata.description = None
+    table["country"].metadata.unit = ""
+    table["country"].metadata.origins = regions_origin
+    table["year"].metadata.title = "Year"
+    table["year"].metadata.description_short = "Year of observation."
+    table["year"].metadata.description = None
+    table["year"].metadata.unit = ""
+    table["year"].metadata.origins = regions_origin
+
+    ####################################################################################################################
+    if table["gdp"].metadata.description is None:
+        print("WARNING: Column gdp finally has a description_short. Remove this part of the code")
+    else:
+        table["gdp"].metadata.description_short = table["gdp"].metadata.description
+        table["gdp"].metadata.description = None
+
+    if table["population"].metadata.description is None:
+        print("WARNING: Column population has no longer a description field. Remove this part of the code")
+    else:
+        table["population"].metadata.description = None
+
+    ####################################################################################################################
+
+    # Gather column names, titles, short descriptions, unit and origins from the indicators' metadata.
+    metadata = {"column": [], "description": [], "unit": [], "source": []}
     for column in table.columns:
         metadata["column"].append(column)
-        metadata["description"].append(table[column].metadata.description)
-        metadata["source"].append(table[column].metadata.sources[0].name)
+
+        if hasattr(table[column].metadata, "description") and table[column].metadata.description is not None:
+            print(f"WARNING: Column {column} still has a 'description' field.")
+        # Prepare indicator's description.
+        description = ""
+        if hasattr(table[column].metadata.presentation, "title_public") and table[column].metadata.presentation.title_public is not None:
+            description += table[column].metadata.presentation.title_public
+        else:
+            description += table[column].metadata.title
+        if table[column].metadata.description_short:
+            description += f" - {table[column].metadata.description_short}"
+            description = remove_details_on_demand(description)
+        metadata["description"].append(description)
+
+        # Prepare indicator's unit.
+        if table[column].metadata.unit is None:
+            print(f"WARNING: Column {column} does not have a unit.")
+            unit = ""
+        else:
+            unit = table[column].metadata.unit
+        metadata["unit"].append(unit)
+
+        # Gather unique origins of current variable.
+        unique_sources = []
+        for origin in table[column].metadata.origins:
+            # Construct the source name from the origin's attribution.
+            # If not defined, build it using the default format "Producer - Data product (year)".
+            source_name = (
+                origin.attribution
+                or f"{origin.producer} - {origin.title or origin.title_snapshot} ({origin.date_published.split('-')[0]})"
+            )
+
+            # Add url at the end of the source.
+            if origin.url_main:
+                source_name += f" [{origin.url_main}]"
+
+            # Add the source to the list of unique sources.
+            if source_name not in unique_sources:
+                unique_sources.append(source_name)
+
+        # Concatenate all sources.
+        sources_combined = "; ".join(unique_sources)
+        metadata["source"].append(sources_combined)
 
     # Create a dataframe with the gathered metadata and sort conveniently by column name.
     codebook = pd.DataFrame(metadata).set_index("column").sort_index()
     # For clarity, ensure column descriptions are in the same order as the columns in the data.
     first_columns = ["country", "year", "iso_code", "population", "gdp"]
     codebook = pd.concat(
-        [codebook.loc[first_columns], codebook.drop(first_columns)]
+        [codebook.loc[first_columns], codebook.drop(first_columns, errors="raise")]
     ).reset_index()
 
     return codebook
+
+
+def load_latest_dataset(dataset_name: str = "owid_energy", namespace: str="energy",
+                        path_to_local_catalog: str = "../etl/data/", channel:str = "garden") -> Table:
+    try:
+        # First try to load the latest OWID energy dataset from the local catalog, if it exists.
+        tables = (
+            LocalCatalog(path_to_local_catalog, channels=[channel])
+            .find(dataset_name, namespace=namespace)
+            .sort_values("version", ascending=False)
+        )
+    except ValueError:
+        # Load the latest OWID energy dataset from the remote catalog.
+        tables = find(
+            dataset_name, namespace=namespace, channels=[channel]
+        ).sort_values("version", ascending=False)
+    table_selected = tables.iloc[0]
+    tb = table_selected.load()
+    print(f"Loaded: {table_selected.path}")
+
+    return tb
 
 
 def main() -> None:
     #
     # Load data.
     #
-    # Load OWID-energy dataset from the catalog.
-    table = catalog.find_latest(
-        ENERGY_DATASET_TABLE, namespace="energy", channels=["garden"]
-    )
+    # Load latest dataset from etl (from a local or otherwise a remote catalog).
+    # NOTE: If the latest dataset exists but is not found, run "reindex" from the etl poetry shell.
+    tb = load_latest_dataset()
 
     #
     # Process data.
     #
     # Minimum processing of the data.
-    df = prepare_data(table=table)
+    tb = prepare_data(tb=tb)
 
     # Prepare codebook.
-    codebook = prepare_codebook(table=table)
+    codebook = prepare_codebook(tb=tb)
 
     # Sanity check.
     error = "Codebook column descriptions are not in the same order as data columns."
-    assert codebook["column"].tolist() == df.columns.tolist(), error
+    assert codebook["column"].tolist() == tb.columns.tolist(), error
 
     #
     # Save outputs.
     #
-    # Save dataset to files in different formats.
-    df.to_csv(OUTPUT_CSV_FILE, index=False, float_format="%.3f")
-    df.to_excel(OUTPUT_EXCEL_FILE, index=False, float_format="%.3f")
-    df_to_json(df, OUTPUT_JSON_FILE, ["iso_code"])
-
+    # Save data to a csv file.
+    # NOTE: First convert to dataframe to avoid saving metadata as an additional json file.
+    pd.DataFrame(tb).to_csv(OUTPUT_CSV_FILE, index=False, float_format="%.3f")
+    # Save data and codebook to an excel file.
+    with pd.ExcelWriter(OUTPUT_EXCEL_FILE) as writer:
+        tb.to_excel(writer, sheet_name='Data', index=False, float_format="%.3f")
+        codebook.to_excel(writer, sheet_name='Metadata')
+    # Save data to json.
+    save_data_to_json(tb, OUTPUT_JSON_FILE)
     # Save codebook file.
     codebook.to_csv(CODEBOOK_FILE, index=False)
 
